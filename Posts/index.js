9cc1ba3b96f7e750583f203362363592621da775
irene-nakabugo <irenenakabugo0@gmail.com>
2023-09-22 11:57:29 +0300

    Nodemon & server

--- Posts/index.js
+++ Posts/index.js
@@ -4,6 +4,15 @@ const express = require('express');
 const app=express();
 
 //building a server
-const port = 4000;
+const port = 7000;
+
+//app.listen(port_number, function)
+
+app.listen(port, ()=>{console.log("Listening at port 7000")} )
 
 console.log('Backend programming')
+
+//CTRL + C
+
+//CRUD operations
+//Create, Read, Update, Delete
